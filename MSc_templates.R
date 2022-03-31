ecdfe638c93b75bd323c181e8cabad1bdb022538
helenesandsten <sandstenhelene@gmail.com>
2022-03-31 08:32:08 +0200

    added filnames-template

--- MSc_templates.R
+++ MSc_templates.R
@@ -1,4 +1,25 @@
-### FIGURE TEMPLATES
+###### TEMPLATES AND HOW TO ORGANIZE STUFF #####
+
+#### NAMING ---------------------------------------------------------
+#### FILE NAMES
+
+### MANUSCRIPT
+# MSc_HS_Manuscript_YYYY.MM.DD.Rmd
+
+### SIGNLE SCRIPTS
+# ThreeD_MSc_DATASET_import.R
+# ThreeD_MSc_DATASET_fixup.R
+# ThreeD_MSc_DATASET_figures.R
+# ThreeD_MSc_DATASET_analysis.R
+
+#   eks: ThreeD_MSc_roots_figures.R
+#   eks: ThreeD_MSc_soil_analysis.R 
+
+
+
+
+
+### FIGURES ---------------------------------------------------------
 
 # SCATTER PLOT
 plotname <- ggplot(data = DF, aes(x = , y = )) +
@@ -19,14 +40,14 @@ ylab(label)
 
 ### FILTERS
 # # only warming 
-#   filter(grazing == "C") %>% 
-#   filter(Nlevel == "1" & "2" & "3") %>% 
+# filter(grazing == "C") %>% 
+# filter(Nlevel == 1 & 2 & 3) %>% 
 # 
 # # only nitrogen
-#   filter(warming == "A") %>% 
-#   filter(grazing == "C") %>% 
+# filter(warming == "A") %>% 
+# filter(grazing == "C") %>% 
 # 
 # # only grazing 
-#   filter(warming == "A") %>% 
-#   filter(Nlevel == "1" & "2" & "3") %>% 
+# filter(warming == "A") %>% 
+# filter(Nlevel == "1" & "2" & "3") %>% 
     
