dfba55bbdf5670452e6ef22ae061645d8115d933
helenesandsten <sandstenhelene@gmail.com>
2022-03-31 10:40:41 +0200

    aud helped fix scripts

--- ThreeD_MSc_roots_figures.R
+++ ThreeD_MSc_roots_figures.R
@@ -4,13 +4,13 @@ source("ThreeD_MSc_roots_fixup.R")
 
 # plot of rootmass and RIC depth
 roots_plot1 <- ggplot(data = roots.df, 
-                      aes(x = root_mass_g, y = RIC_length_cm), 
+                      aes(x = RIC_length_cm, y = root_mass_g), 
                       color = warming) + 
   geom_point(size = 3, alpha = 0.5) +
   theme_bw(base_size = 20) +
   labs(title = "Root mass and RIC length", 
-       x = "Root mass (g)",
-       y = "RIC length (cm)")
+       x = "RIC length (cm)",
+       y = "Root mass (g)")
 roots_plot1
 
 
@@ -18,40 +18,48 @@ roots_plot1
 plot_roots_w <- roots.df %>% 
   filter(grazing == "C") %>% 
   filter(Nlevel == 1 & 2 & 3) %>% 
-  ggplot(data = roots.df, mapping = aes(x = warming, y = root_mass_g, fill = warming)) +
-  geom_boxplot(fill = c("#fecc5c", "#fd8d3c")) +
+  ggplot(data = roots.df, mapping = aes(x = warming, y = root_mass_g)) +
+  geom_boxplot() +#fill = c("#fecc5c", "#fd8d3c")) +
   theme_bw(base_size = 20) +
-  labs(title = "Effect of warming on root growth", x = "warming", y = "Root mass (g)") 
+  labs(title = "Effect of warming on root growth", x = "warming", y = "Root mass (g)")+ 
+  facet_wrap(~origSiteID) 
 plot_roots_w
 
 # plot root biomass - nitrogen (kg)
 labels_nlevel <- c("0", "0", "0", "0.5", "1", "5", "10", "50", "100", "150")
 
-plot_roots_nkg <- roots.df %>% 
+plot_roots_namount <- roots.df %>% 
   filter(warming == "A") %>% 
   filter(grazing == "C") %>% 
-  ggplot(data = roots.df, 
-         mapping = aes(x = Nlevel, y = root_mass_g, 
-                       group = Nlevel, fill = Nlevel)) +
-  geom_boxplot(fill = "cornflowerblue") +
+  group_by(Namount_kg_ha_y, origSiteID) %>% 
+  summarise(root_mass_g = mean(root_mass_g)) %>% 
+  ggplot(mapping = aes(x = log(Namount_kg_ha_y +1), y = root_mass_g, color = origSiteID)) +
+                       #group = Nlevel, fill = Nlevel)) +
+  geom_point() +
   theme_bw(base_size = 20) +
   labs(title = "Effect of Nitrogen on root growth", 
-       x = "N kg/ha/y", 
-       y = "Root mass (g)") +
-  scale_x_discrete(labels = labels_nlevel)
-plot_roots_nkg
+       x = bquote(log(Nitrogen)~(kg~ha^-1~y^-1)), 
+       y = "Root mass (g)") 
+  #scale_x_discrete(labels = labels_nlevel)
+plot_roots_namount
 
 # plot root biomass - nitrogen (levels)
-plot_roots_nlvl <- roots.df %>% 
-  filter(warming == "A") %>% 
-  filter(grazing == "C") %>% 
-  ggplot(data = roots.df, 
-         mapping = aes(x = Nlevel, y = root_mass_g,
-                       group = Nlevel, fill = Nlevel)) +
-  geom_boxplot(fill = "cornflowerblue") +
-  theme_bw(base_size = 20) +
-  labs(title = "Effect of Nitrogen on root growth", x = "N level", y = "Root mass (g)") 
-plot_roots_nlvl
+# plot_roots_nlvl <- roots.df %>% 
+#   filter(warming == "A") %>% 
+#   filter(grazing == "C") %>% 
+#   ggplot(data = roots.df, 
+#          mapping = aes(x = Nlevel, y = root_mass_g,
+#                        group = Nlevel, fill = Nlevel)) +
+#   geom_boxplot(fill = "cornflowerblue") +
+#   theme_bw(base_size = 20) +
+#   labs(title = "Effect of Nitrogen on root growth", x = "N level", y = "Root mass (g)") 
+# plot_roots_nlvl
+
+## aud helps: move this to fixup script
+roots.df <- roots.df %>% 
+  left_join(NitrogenDictionary, by = "Nlevel")
+
+
 
 
 # plot root biomass - grazing 
@@ -60,8 +68,7 @@ labels_grz <- c("control", "intensive", "medium", "natural")
 plot_roots_grz <- roots.df %>% 
   filter(warming == "A") %>% 
   filter(Nlevel == 1 & 2 & 3) %>% 
-  ggplot(data = roots.df, 
-         mapping = aes(x = grazing, y = root_mass_g, fill = grazing)) +
+  ggplot(mapping = aes(x = grazing, y = root_mass_g, fill = grazing)) +
   geom_boxplot() +
   theme_bw(base_size = 20) +
   labs(title = "Effect of grazing on root growth", 
@@ -72,7 +79,19 @@ plot_roots_grz <- roots.df %>%
 plot_roots_grz
 
   
-  
+## megaplot roots
+plot_roots_mega <- roots.df %>% 
+  group_by(Namount_kg_ha_y, origSiteID, warming, grazing) %>% 
+  summarise(root_mass_g = mean(root_mass_g)) %>% 
+  ggplot(mapping = aes(x = log(Namount_kg_ha_y +1), y = root_mass_g, color = warming)) +
+  geom_point() +
+  theme_bw(base_size = 20) +
+  labs(title = "Effect of Nitrogen on root growth", 
+       x = bquote(log(Nitrogen)~(kg~ha^-1~y^-1)), 
+       y = "Root mass (g)") +
+  facet_grid(origSiteID ~ grazing) + 
+  geom_smooth(method = "lm")
+plot_roots_mega
 
   
   
