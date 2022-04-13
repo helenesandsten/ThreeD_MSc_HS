b7608d6dbc97c347dc103532b9d41cd9ee714635
helenesandsten <sandstenhelene@gmail.com>
2022-04-13 16:20:35 +0200

    fixed colors

--- R scripts/MSc_aesthetics.R
+++ R scripts/MSc_aesthetics.R
@@ -4,12 +4,17 @@
 # theme, size, 
 
 ## colors for figures
+# scale_fill_manual(values = ___)
 
-# grazing 
+# colors for grazing 
+colors_g <- c("Control" = "#8FAADC", "Intensive" = "#F8CBAD", 
+              "Medium" = "#FFE699", "Natural" = "#A9D18E")
 
-# Nlevels 8
+# colors for warming 
+colors_w <- c("Ambient" = "orange", "Warmed" = "red")
 
-# warming 
+# colors_n 
+#scale_fill_viridis(option = "mako") 
 
 
 
