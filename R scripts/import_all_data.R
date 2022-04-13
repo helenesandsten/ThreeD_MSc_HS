0fbaa4a53aa86006c5411f23d384fcf71eaa665a
helenesandsten <sandstenhelene@gmail.com>
2022-04-13 16:22:34 +0200

    no change

---
+++ R scripts/import_all_data.R
@@ -0,0 +1,20 @@
+### FUTURE DOWNLOAD PLAN
+
+## Data dowloaded from osf
+
+##### ABOVEGROUND BIOMASS
+## import dataset 
+
+
+
+##### BELOWGROUND BIOMASS - ROOTS
+## import dataset (excel) 
+roots.raw.df <- read_excel("Data/ThreeD_rootingrowthcores_2021.xlsx") 
+
+##### DECOMPOSITION - TEABAGS
+## import dataset 
+
+
+
+##### SOIL DATA
+## import dataset 
