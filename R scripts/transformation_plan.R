e3ed4631edcb468296d625135bf8403e32f65d37
helenesandsten <sandstenhelene@gmail.com>
2022-04-13 16:22:07 +0200

    cleaned old stuff out

--- R scripts/transformation_plan.R
+++ R scripts/transformation_plan.R
@@ -1,10 +1,10 @@
 ## transformation plan 
 
-source("R scripts/ThreeD_load_packages.R")
-source("R scripts/ThreeD_create_metadata.R")
+# source("R scripts/ThreeD_load_packages.R")
+# source("R scripts/ThreeD_create_metadata.R")
 source("R scripts/download_plan.R")
 
-tranformation_plan <- list(
+#tranformation_plan <- list(
   
   ### ROOTS / BELOWGROUND BIOMASS --------------------------------
   
@@ -12,18 +12,7 @@ tranformation_plan <- list(
   tar_target(
     name = roots_clean_df,
     command = {
-      # clean data
-      roots.df <- roots.raw.df %>%
-        mutate(dateRIC_washed = ymd(dateRIC_washed),  # change format to date
-               date_roots_dried = ymd(date_roots_dried), # change format to date
-               root_mass_g = total_mass_g - alutray_mass_g, # new column with root mass
-               root_mass_cm3 = (root_mass_g/(pi*(1.6)^2*RIC_length_cm))) %>% # calculate volume
-        mutate_if(is.character, as.factor) %>%
-        left_join(NitrogenDictionary, by = "Nlevel") 
-        # make log(Namount_kg_ha_y +1) into factor
-
-
-          
+   
     })
   
   ### ABOVEGROUND BIOMASS -------------------------------------
