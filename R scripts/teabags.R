81e0b59fa164ac0503e7de9df7b8ac6a3f972f55
helenesandsten <sandstenhelene@gmail.com>
2022-04-21 16:49:50 +0200

    cleaned data, started TBI calculations

--- R scripts/teabags.R
+++ R scripts/teabags.R
@@ -12,24 +12,57 @@ teabag.df <- teabag.raw.df %>%
   mutate(origBlockID = as.factor(origBlockID),
          origPlotID = as.factor(origPlotID),
          destBlockID = as.factor(destBlockID),
-         destPlotID = as.factor(destPlotID)) %>% 
-  mutate(Namount_kg_ha_y = log(Namount_kg_ha_y +1))  # for better visualization
-
-## analysis ---> analysis_plan ---------------------------------------
-## calculating days buried 
-tb.days.buried <- teabag.df %>% 
+         destPlotID = as.factor(destPlotID),
+         tea_type = as.logical(tea_type)) %>% 
+  mutate(Namount_kg_ha_y = log(Namount_kg_ha_y +1)) %>% # for better visualization
+  # remove teabag weight 
+  # mutate(preburial_weight_g = preburial_weight_g - XXX,
+  #        post_burial_weight_g = post_burial_weight_g - XXX)
+  # filter out plots with damaged teabags
+  mutate(comment_3 = ifelse(is.na(comment_2), 1, 0)) %>% 
+  group_by(teabag_ID) %>% 
+  mutate(comment_3 = sum(comment_3)) %>% 
+  filter(comment_3 == 2) %>% 
+  # calculating days buried 
   mutate(days_buried = recover_date - burial_date)
 
+
+## analysis ---> analysis_plan ---------------------------------------
+
+## Equations and numbers from Keuskamp et al. 2013
 ## calculating decomposition rates
 
+h_g <- 0.842 # hydrolyzable fraction green tea 
+h_r <- 0.552 # hydrolyzable fraction rooibos/red tea 
 
-t <- tb.days.buried$days_buried
-w <- tb.days.buried$post_burial_weight_g
-#a <- 
-# decomposition rate of labile fraction 
-k1 <- 
-# decomposition rate of recalcitrant fraction 
-k2 <- 
+
+
+tbi.g.df <- teabag.df %>% # decomposable fraction green tea 
+  mutate(a_g = case_when(tea_type ~ "green",
+                         TRUE ~ "red")) %>% 
+  mutate(s = 1 - a_g/post_burial_weight_g) 
+  
+
+  # mutate(filter(tea_type == "green"), a_g = post_burial_weight_g)
+  # 
+  # filter(tea_type == "green") %>%  
+  # select(post_burial_weight_g) %>% 
+  # mutate(S = 1 - post_burial_weight_g/H_g) 
+
+
+# 
+
+
+
+
+
+# t <- tb.days.buried$days_buried
+# w <- tb.days.buried$post_burial_weight_g
+# #a <- 
+# # decomposition rate of labile fraction 
+# k1 <- 
+# # decomposition rate of recalcitrant fraction 
+# k2 <- 
 
 
 ## figures ---> figure_plan ---------------------------------------
