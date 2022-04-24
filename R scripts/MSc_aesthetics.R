dfbb5465f22ba0888719a0f909681ae67409983d
helenesandsten <sandstenhelene@gmail.com>
2022-04-24 18:35:51 +0200

    changed warming colors

--- R scripts/MSc_aesthetics.R
+++ R scripts/MSc_aesthetics.R
@@ -11,7 +11,7 @@ colors_g <- c("Control" = "#8FAADC", "Intensive" = "#F8CBAD",
               "Medium" = "#FFE699", "Natural" = "#A9D18E")
 
 # colors for warming 
-colors_w <- c("Ambient" = "orange", "Warmed" = "red")
+colors_w <- c("Ambient" = "blue", "Warmed" = "red")
 
 # colors_n 
 #scale_fill_viridis(option = "mako") 
