67cfc3e33d020c362a62735de101869e32946f96
helenesandsten <sandstenhelene@gmail.com>
2022-04-26 14:17:18 +0200

    added clean_output naming for tables

--- R scripts/MSc_aesthetics.R
+++ R scripts/MSc_aesthetics.R
@@ -19,3 +19,27 @@ colors_w <- c("Ambient" = "blue", "Warmed" = "red")
 # colors for teabags
 colors_tea <- c("olivedrab4", "orangered3")
 
+# better table names 
+# clean_output.DATA.XXX <- output.DATA.XXX %>%
+#   mutate(term = case_when(
+#     (term == "(Intercept)") ~ "Intercept (no N, not warmed, not grazed)",
+#     (term == "Namount_kg_ha_y") ~ "Nitrogen",
+#     (term == "warmingWarmed") ~ "Warmed",
+#     (term == "grazingIntensive") ~ "Intensive grazing",
+#     (term == "grazingMedium") ~ "Medium grazing",
+#     (term == "grazingNatural") ~ "Natural grazing",
+#     (term == "Namount_kg_ha_y:warmingWarmed") ~ "Nitrogen : Warmed",
+#     (term == "Namount_kg_ha_y:grazingIntensive") ~ "Nitrogen : Intensive grazing",
+#     (term == "Namount_kg_ha_y:grazingMedium") ~ "Nitrogen : Medium grazing",
+#     (term == "Namount_kg_ha_y:grazingNatural") ~ "Nitrogen : Natural grazing",
+#     (term == "warmingWarmed:grazingIntensive") ~ "Warmed : Intensive grazing",
+#     (term == "warmingWarmed:grazingMedium") ~ "Warmed : Medium grzing",
+#     (term == "warmingWarmed:grazingNatural") ~ "Warmed : Natural grazing",
+#     (term == "Namount_kg_ha_y:warmingWarmed:grazingIntensive") ~ 
+#       "Nitrogen : Warmed : Intensive grazing",
+#     (term == "Namount_kg_ha_y:warmingWarmed:grazingMedium") ~ 
+#       "Nitrogen : Warmed : Medium grazing",
+#     (term == "Namount_kg_ha_y:warmingWarmed:grazingNatural") ~ 
+#       "Nitrogen : Warmed : Natural grazing"
+#   ))
+
