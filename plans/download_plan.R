ba39b5b0b226689a18f7db607a1b194638576e2f
helenesandsten <sandstenhelene@gmail.com>
2022-05-21 21:54:24 +0200

    added agb

--- plans/download_plan.R
+++ plans/download_plan.R
@@ -1,15 +1,20 @@
 ### download plan 
 
+source("R scripts/ThreeD_load_packages.R")
+source("R scripts/ThreeD_create_metadata.R")
+source("R scripts/MSc_aesthetics.R")
+
 ## Data dowloaded from osf
 
 ##### ABOVEGROUND BIOMASS
 ## import dataset 
-
+## importing data -----------------------------------
+agb.raw.df <- read_csv("Data/THREE-D_clean_biomass_2020-2021.csv")
 
 
 ##### BELOWGROUND BIOMASS - ROOTS
 ## import dataset (excel) 
-roots.raw.df <- read_excel("Data/ThreeD_rootingrowthcores_2021.xlsx") 
+
 
 
 ##### DECOMPOSITION - TEABAGS
