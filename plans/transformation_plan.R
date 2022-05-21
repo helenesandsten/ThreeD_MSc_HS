d81d0612738e7a30109b897479d7ca0581708da4
helenesandsten <sandstenhelene@gmail.com>
2022-05-21 21:54:18 +0200

    added agb

--- plans/transformation_plan.R
+++ plans/transformation_plan.R
@@ -6,40 +6,52 @@ source("R scripts/download_plan.R")
 
 #tranformation_plan <- list(
   
-  ### ROOTS / BELOWGROUND BIOMASS --------------------------------
-  
-  ## import root biomass and fix it
+
+  ## ABOVEGROUND BIOMASS -------------------------------------
+  # import biomass and fix it
   tar_target(
-    name = roots_clean_df,
-    command = {
-   
-    })
+    name = data.agb.df,
+    command = agb.df <- agb.raw.df %>%
+mutate_if(is.character, as.factor) %>%
+  mutate(origBlockID = as.factor(origBlockID),
+         origPlotID = as.factor(origPlotID),
+         destBlockID = as.factor(destBlockID),
+         destPlotID = as.factor(destPlotID)) %>% 
+  mutate(Namount_kg_ha_y = log(Namount_kg_ha_y +1)) %>% 
+  # calculating biomass per m2 to standardize amounts
+  mutate(biomass_m2 = ((biomass / area_cm2) * 10000)) %>% 
+  filter(year == 2021) %>%
+  filter(turfID != "147 WN9C 194") %>%# removing plots with missing forbs biomass
+  # renaming variables 
+  mutate(grazing = recode(grazing, 
+                          "C" = "Control", "I" = "Intensive", 
+                          "M" = "Medium", "N" = "Natural"),
+         warming = recode(warming, 
+                          "A" = "Ambient", "W" = "Warming")) %>% 
+  # reordering variables 
+  mutate(origSiteID = factor(origSiteID, levels = c("Lia", "Joa")),
+         grazing = factor(grazing, 
+                          levels = c("C" = "Control", 
+                                     "M" = "Medium",
+                                     "I" = "Intensive",
+                                     "N" = "Natural"))) %>% 
+  # changing grazing into cont. variable too reduce degrees of freedom in models
+  mutate(grazing_lvl = case_when((grazing == "Control") ~ 0,
+                                 (grazing == "Medium") ~ 2,
+                                 (grazing == "Intensive") ~ 4))
   
-  ### ABOVEGROUND BIOMASS -------------------------------------
-  # # import biomass and fix it
-  # tar_target(
-  #   name = data_agbiomass,
-  #   command = agb.df <- read_csv("Data/THREE-D_clean_biomass_2020-2021.csv", col_names = TRUE,
-  #                                cols(
-  #                                  .default = col_double(),
-  #                                  origSiteID = col_factor(),
-  #                                  origBlockID = col_factor(),
-  #                                  turfID = col_character(),
-  #                                  destSiteID = col_factor(),
-  #                                  destBlockID = col_factor(),
-  #                                  Namount_kg_ha_y = col_factor(),
-  #                                  warming = col_factor(),
-  #                                  grazing = col_factor(),
-  #                                  date = col_datetime(format = ""),
-  #                                  fun_group = col_factor(),
-  #                                  unit = col_character(),
-  #                                  collector = col_character(),
-  #                                  remark = col_character()
-  #                                )) %>% 
-  #     filter(year == 2021)
-  #     )
-  # 
   
+
+### ROOTS / BELOWGROUND BIOMASS --------------------------------
+
+## import root biomass and fix it
+# tar_target(
+#   name = roots_clean_df,
+#   command = {
+#  
+#   })
+
+
   ### TEABAG / DECOMPOSITION BIOMASS --------------------------------
   
   # # import teabag data and fix it
