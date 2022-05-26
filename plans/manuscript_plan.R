a947f5dda18412d1354f1cd4d16a9e261ef9a99d
helenesandsten <sandstenhelene@gmail.com>
2022-05-26 18:04:33 +0200

    added target manucript stuff from aud

--- plans/manuscript_plan.R
+++ plans/manuscript_plan.R
@@ -1,3 +1,22 @@
 ## manuscript plan 
 
-#manuscript_plan <- list( 
+manuscript_plan <- list(
+  # bibliography
+  tar_target(
+    name = bibliography,
+    command = "manuscript/mybibfile.bib",
+    format = "file"
+  ),
+  # add packages to bibliography
+  # tar_target(
+  #   name = biblio2,
+  #   command = rjt.misc::package_citations(
+  #       packages = c("targets", "tidyverse", "PFTCFunctions",  "googlesheets4"),
+  #       old_bib = bibliography,
+  #       new_bib = "manuscript/mybibfile2.bib"),
+  #   format = "file"
+  #   ),
+  
+  #render manuscript
+  tar_render(name = ms, path = "manuscript/manuscript.Rmd")
+)
